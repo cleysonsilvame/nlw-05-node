1d1a47a3728f9621f12219aa0c8c7059e481ae45
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-04-28 03:43:36 -0300

    aula-03

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersControllers';
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesControllers = new MessagesController();
 
 const routes = Router();
 
 routes.post('/settings', settingsController.create);
+
+routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesControllers.create);
+routes.get('/messages/:id', messagesControllers.showByUser);
 
 export { routes };
